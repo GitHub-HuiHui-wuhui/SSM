b31e332de2bc91450ecb0b27c4a21e48180bbd0b
无悔 <2418861250@qq.com>
2023-02-22 00:44:14 +0800

    commit 2

--- mybatis_resultMap/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
+++ mybatis_resultMap/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
@@ -3,6 +3,8 @@ package com.atguigu.mybatis.mapper;
 import com.atguigu.mybatis.pojo.Emp;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface EmpMapper {
 
     /**
@@ -25,4 +27,11 @@ public interface EmpMapper {
      * @return
      */
     Emp getEmpAndDeptByStepOne(@Param("empId") Integer empId);
+
+    /**
+     * 通过分步查询查询部门以及部门中的员工信息的第二步
+     * @param deptId
+     * @return
+     */
+    List<Emp> getDeptAndEmpByStepTwo(@Param("deptId") Integer deptId);
 }
